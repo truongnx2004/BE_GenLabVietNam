c425903cddf49f817c80d04559e5c06130f9288c
Cao Phuc <cphuc2004@gmail.com>
2025-07-31 05:43:04 -0700

    Cập nhật thêm bookingController và StaffBookingController

---
+++ BE/src/routes/bookingRoutes.js
@@ -0,0 +1,18 @@
+﻿const express = require('express');
+const router = express.Router();
+const bookingController = require('../controllers/bookingController');
+
+// Tạo booking mới
+router.post('/create', bookingController.createBooking);
+
+// Lấy danh sách booking hoặc chi tiết 1 booking
+router.get("/", bookingController.getAllBookings);
+router.get("/:id", bookingController.getBookingById);
+
+// ✅ Cập nhật toàn bộ đơn (địa chỉ, giờ, v.v.)
+router.put("/:id", bookingController.updateBooking);
+
+// Lấy giá tiền đơn
+router.get("/:id/price", bookingController.getBookingPrice);
+
+module.exports = router;
---
+++ BE/src/routes/staffSampleRoutes.js
@@ -0,0 +1,11 @@
+﻿const express = require("express");
+const router = express.Router();
+const sampleController = require("../controllers/staffSampleController");
+
+// Lấy tất cả sample
+router.get("/", sampleController.getAllSamplesWithInfo);
+
+// Cập nhật sample
+router.put("/:id", sampleController.updateSample);
+
+module.exports = router;
